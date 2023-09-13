708e53d67158c21f114614d65dba01f3343b8b9f
panduzephyr <107347103+panduzephyr@users.noreply.github.com>
2023-09-13 15:05:16 +0530

    main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,10 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+    region = "ap-south-1"  # Set your desired AWS region
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-0f5ee92e2d63afc18"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    subnet_id = "subnet-0e3653efb9a21e3c4"
+    key_name = "Linux"
 }
